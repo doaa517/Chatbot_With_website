9efb50461ba9d700fa38719d07b7a269e29c0c98
D4sh12 <mayarkulthum@gmail.com>
2022-03-23 23:46:36 +0200

    remove  action_save_name

--- django_rasa_bot-master/rasabot/actions/actions.py
+++ django_rasa_bot-master/rasabot/actions/actions.py
@@ -213,35 +213,6 @@ class DbQueryingMethods:
 
 
 
-
-
-
-class ActionGreet(Action):
-    def name(self):
-        return "action_save_name"
-
-    def run(self, dispatcher, tracker, domain):
-        intros = ["im ", "i am ", "i am called ", "im called ", "call me "]
-        username = ""
-        stripped = ""
-        sentence = (tracker.latest_message)['text']
-        exclude = set(string.punctuation)
-        stripped = stripped.join(ch for ch in sentence if ch not in exclude).lower()
-        for intro in intros:
-            if re.search(intro, stripped):
-                username = stripped.split(intro)[1]
-                username = username.title()
-        if username is "":
-            username = sentence.title()
-        logging.debug("*** ActionGreet: Name saved as: " + username)
-        return [SlotSet("username", username)]
-
-
-
-
-
-
-
 class DbQueryingMethodsDegree:
     def create_connection(db_file):
         """ 
